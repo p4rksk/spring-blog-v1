94fa5134bc0dd63f0cd8368bdb5308a3bd88f595
yeeun0118s <yeeun0118s@naver.com>
2024-02-02 17:49:13 +0900

    v2 만드는중

--- src/main/java/shop/mtcoding/blog/board/Board.java
+++ src/main/java/shop/mtcoding/blog/board/Board.java
@@ -1,2 +1,25 @@
-package shop.mtcoding.blog.board;public class Board {
+package shop.mtcoding.blog.board;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import shop.mtcoding.blog.user.User;
+
+import java.sql.Timestamp;
+
+@Data // SET, GET, ToString
+@Entity // 깃발을 꼽는 으로 이해하면될듯
+@Table(name = "board_tb") // 테이블 이름 설정
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String content;
+
+    @ManyToOne
+    private User user;
+
+    @CreationTimestamp
+    private Timestamp createdAt;
 }
--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -1,2 +1,21 @@
-package shop.mtcoding.blog.board;public class BoardRepository {
+package shop.mtcoding.blog.board;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class BoardRepository {
+
+    private final EntityManager em;
+
+    public Board findById(int id){
+        Query query = em.createQuery("select b from Board b join fetch b.user u where b.id = :id", Board.class); // jpa
+        query.setParameter("id", id);
+
+        Board board = (Board)query.getSingleResult();
+        return board;
+    }
 }
